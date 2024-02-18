256f332611bae970faac1b3c6d3a48a4fc003be3
1337kerosene <TranenPeter@gmail.com>
2024-02-18 19:59:05 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+import './style.css';
+import smLogo from '/sm.svg';
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://forms.gle/ppxEJgGdZW6iskdq7" target="_blank">
+      <img src="${smLogo}" class="logo" alt="Star Materia logo" />
+    </a>
+    <h1>STAR MATERIA</h1>
+    <p>Developing the worlds first continuous lactate monitor for athletes. Train better by following your body's internal load and 
+    track your progress on our app with AI integration. Stay tuned for more updates. </p>
+    <p class="read-the-docs">
+      Click on the Star Materia logo to learn more
+    </p>
+  </div>
+`;
