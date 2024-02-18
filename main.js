090ed22ef488a48bd3b753270360e19dacbc1cc2
1337kerosene <tranenpeter@gmail.com>
2024-02-18 20:17:21 +0100

    test

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ document.querySelector('#app').innerHTML = `
     </a>
     <h1>STAR MATERIA</h1>
     <p>Developing the worlds first continuous lactate monitor for athletes. Train better by following your body's internal load and 
-    track your progress on our app with AI integration. Stay tuned for more updates. </p>
+    track your progress on our app with AI integrationd. Stay tuned for more updates. </p>
     <p class="read-the-docs">
       Click on the Star Materia logo to learn more
     </p>
